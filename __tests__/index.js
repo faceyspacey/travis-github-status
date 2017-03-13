bb6680e073b39f1ffc94ce7a3b49eef004fc7efc
James Gillmore <james@faceyspacey.com>
2017-03-13 05:55:50 -0700

    npm colors applied

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,3 +3,15 @@ import { shouldSet } from '../src'
 it('it works', () => {
   expect(shouldSet('foo')).toEqual(false)
 })
+
+it('it does NOT work', () => {
+  expect(shouldSet('foo')).toEqual(true)
+})
+
+it('it works3', () => {
+  expect(shouldSet('foo')).toEqual(false)
+})
+
+it('it does NOT work3', () => {
+  expect(shouldSet('foo')).toEqual(true)
+})
